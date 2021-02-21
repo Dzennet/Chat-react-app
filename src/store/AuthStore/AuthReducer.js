526aca429aec189a323123355e98036c0eaf8ee2
Dzennet <andy.biersack.2014@mail.ru>
2021-02-21 20:47:46 +0300

    Add changes

--- src/store/AuthStore/AuthReducer.js
+++ src/store/AuthStore/AuthReducer.js
@@ -3,7 +3,7 @@ import { TOGGLE_IS_AUTH, UPDATE_AVATAR } from "./AuthConstants";
 let initState = {
   login: "email@mail.ru",
   password: "qwerty12345",
-  isAuth: true,
+  isAuth: false,
   avatar: null,
 };
 
